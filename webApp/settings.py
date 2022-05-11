db39358525144d26381e6a4fad2620b37e1049c5
edgzoah <adamoblady@gmail.com>
2022-05-11 23:31:52 +0200

    .

--- webApp/settings.py
+++ webApp/settings.py
@@ -1,8 +1,7 @@
 from lib import *
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'F3HUIF23H8923F9H8389FHXKLN'
-app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'webApp', 'static', 'images')
+app.config['UPLOAD_FOLDER'] = os.path.join(os.path.realpath(__file__).replace(os.path.basename(os.path.realpath(__file__)), ''), 'static', 'images')
 
 login_manager = LoginManager()
 login_manager.login_view = "login"
