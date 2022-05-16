ce6b947170b4c6c6bdae3606807aa750f66da7cd
Tomasz Ogrodnik <togrodnik2006@gmail.com>
2022-05-16 22:14:19 +0200

    new Style,s repired index.js

--- webApp/static/scripts/header.js
+++ webApp/static/scripts/header.js
@@ -1,26 +1,30 @@
 function chceckTa() {
-  if (window.matchMedia("(max-width: 700px)").matches) {
-  console.log("ta?");
-  }
-  else {
-    window.onscroll = function () {
+  window.onscroll = function () {
       if (niewiem.x == "header") {
             if (window.pageYOffset > 250) {
+              document.getElementById("news-text").className = "news-text start-animation"
+              document.getElementById("news-text").style.display = "block"            
+            } 
+            if (window.pageYOffset > 450) {
               document.getElementById("row-1").style.display = "flex";
-              document.getElementById("news-text").className = "news start-animation";
+              document.getElementById("news-1").className = "news start-animation";
             }
           }
-      if (window.pageYOffset < 50) {
-        document.getElementById("header").style.backgroundColor = "white";
-        document.getElementById("header").style.height = "100px";
-        document.getElementById("logo").style.height = "75px";
+      if (window.matchMedia("(max-width: 700px)").matches) {
+        console.log("niepowiem")
       }
-      if (window.pageYOffset > 0) {
-        document.getElementById("header").style.backgroundColor = "grey";
-        document.getElementById("header").style.height = "50px";
-        document.getElementById("logo").style.height = "45px";
+      else {
+        if (window.pageYOffset < 50) {
+          document.getElementById("header").style.backgroundColor = "white";
+          document.getElementById("header").style.height = "100px";
+          document.getElementById("logo").style.height = "75px";
+        }
+        if (window.pageYOffset > 0) {
+          document.getElementById("header").style.backgroundColor = "grey";
+          document.getElementById("header").style.height = "50px";
+          document.getElementById("logo").style.height = "45px";
+        }
       }
     };
   }
-}
 window.addEventListener("resize", chceckTa());
