f6142772ff0e72d06378e0f14e40605c84824bc6
togrodnik2006@gmail.com <togrodnik2006@gmail.com>
2022-05-18 19:09:54 +0200

    new Styles! Index.html changed

--- webApp/static/scripts/header.js
+++ webApp/static/scripts/header.js
@@ -9,13 +9,11 @@ function chceckTa() {
   window.onscroll = function () {
       if (!window.matchMedia("(max-width: 768px)").matches) {
         if (window.pageYOffset <= 10) {
-          document.getElementById("ph-txt-hide").style.display = "inline";
           document.getElementById("header").style.backgroundColor = "white";
           document.getElementById("header").style.height = "auto";
           document.getElementById("logo").style.height = "75px";
         }
         else {
-          document.getElementById("ph-txt-hide").style.display = "none";
           document.getElementById("header").style.height = "80px";
           document.getElementById("logo").style.height = "65px";
         }
