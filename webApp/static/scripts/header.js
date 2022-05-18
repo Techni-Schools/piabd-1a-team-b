bd01df061570573c458932e782c2d7aed545e84f
togrodnik2006@gmail.com <togrodnik2006@gmail.com>
2022-05-18 16:15:02 +0200

    new Styles!

--- webApp/static/scripts/header.js
+++ webApp/static/scripts/header.js
@@ -1,16 +1,21 @@
 function chceckTa() {
+  if (window.matchMedia("(max-width: 768px)").matches) {
+  
+    document.getElementById("profile-button").onclick = () => {
+      document.getElementById("niepowiem").style.display = "none";
+      window.location.href = "www.google.com";
+    }
+  }
   window.onscroll = function () {
       if (!window.matchMedia("(max-width: 768px)").matches) {
         if (window.pageYOffset <= 10) {
           document.getElementById("ph-txt-hide").style.display = "inline";
-          document.getElementById("ph-txt-hide1").style.display = "inline";
           document.getElementById("header").style.backgroundColor = "white";
           document.getElementById("header").style.height = "auto";
           document.getElementById("logo").style.height = "75px";
         }
         else {
           document.getElementById("ph-txt-hide").style.display = "none";
-          document.getElementById("ph-txt-hide1").style.display = "none";
           document.getElementById("header").style.height = "80px";
           document.getElementById("logo").style.height = "65px";
         }
