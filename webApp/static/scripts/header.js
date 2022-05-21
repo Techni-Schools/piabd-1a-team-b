a91c841d0fdfdbf3aacc71c9683490194685acf0
Tomasz Ogrodnik <togrodnik2006@gmail.com>
2022-05-22 00:45:21 +0200

    cos nie cos dziala

--- webApp/static/scripts/header.js
+++ webApp/static/scripts/header.js
@@ -9,35 +9,31 @@ function chceckTa() {
     document.getElementById("profile-button").onclick = () => {
       document.getElementById("niepowiem").style.display = "none";
       window.location.href = "dashboard";
-    }
+    };
   }
   window.onscroll = function () {
-      if (!phone) {
-        if (window.pageYOffset <= 10) {
-          document.getElementById("header").style.height = "auto";
-          document.getElementById("logo").style.height = "75px";
+    if (!phone) {
+      if (window.pageYOffset <= 10) {
+        document.getElementById("header").style.height = "auto";
+        document.getElementById("logo").style.height = "75px";
+      } else {
+        document.getElementById("header").style.height = "80px";
+        document.getElementById("logo").style.height = "65px";
+      }
+    }
+    try {
+      if (niewiem.x == "header") {
+        if (window.pageYOffset > animationIndexText) {
+          document.getElementById("news-text").className =
+            "news-text start-animation";
+          document.getElementById("news-text").style.display = "block";
         }
-        else {
-          document.getElementById("header").style.height = "80px";
-          document.getElementById("logo").style.height = "65px";
+        if (window.pageYOffset > animationIndexBlocks) {
+          document.getElementById("row-1").style.display = "flex";
+          document.getElementById("news-1").className = "news start-animation";
         }
       }
-      try {
-        if (niewiem.x == "header") {
-          if (phone) { 
-            console.log(animationIndexBlocks)
-          }
-          if (window.pageYOffset > animationIndexText) {
-            document.getElementById("news-text").className = "news-text start-animation"
-            document.getElementById("news-text").style.display = "block"            
-          } 
-          if (window.pageYOffset > animationIndexBlocks) {
-            document.getElementById("row-1").style.display = "flex";
-            document.getElementById("news-1").className = "news start-animation";
-          }
-        }
-      } catch (error) {
-      }
-    };
+    } catch (error) {}
+  };
 }
 window.addEventListener("resize", chceckTa());
