d192423821bbfac53b26a12b8eaaa341273edcb9
Tomasz Ogrodnik <togrodnik2006@gmail.com>
2022-05-21 10:13:54 +0200

    Register second page valid

--- webApp/static/scripts/register.js
+++ webApp/static/scripts/register.js
@@ -1,14 +1,19 @@
 $(document).ready(function () {
   var pageNumber = 1;
   var validPoints = 0;
-
-  //   first  page valid variables 
+  //   first  page valid variables
 
   var firstValid = false;
   var lastnameValid = false;
   var emailValid = false;
   var phoneValid = false;
 
+  // second page valid variables
+  var streetValid = false;
+  var cityValid = false;
+  var zipValid = false;
+  var stateValid = false;
+  var countryValid = false;
   //
 
   function checkPage() {
@@ -26,7 +31,6 @@ $(document).ready(function () {
       $("#0-1").hide();
       $("#0-2").hide();
       $("#0-3").show();
-      $("#nextButton").val() = "submit"
     } else if (pageNumber == 4) {
       $("#register-form").submit();
     }
@@ -34,13 +38,30 @@ $(document).ready(function () {
 
   function pageNext() {
     if (pageNumber <= 5) {
-      if (pageNumber == 1 ) {
-        if (firstValid == true && lastnameValid == true && emailValid == true && phoneValid == true) {
-          pageNumber += 1; 
+      if (pageNumber == 1) {
+        if (
+          firstValid == true &&
+          lastnameValid == true &&
+          emailValid == true &&
+          phoneValid == true
+        ) {
+          pageNumber += 1;
+        }
+      }
+      if (pageNumber == 2) {
+        if (
+          streetValid == true &&
+          cityValid == true &&
+          zipValid == true &&
+          stateValid == true &&
+          countryValid
+        ) {
+          pageNumber += 1;
         }
       }
     }
-    checkPage()
+    console.log(pageNumber);
+    checkPage();
   }
 
   function pageBack() {
@@ -55,53 +76,104 @@ $(document).ready(function () {
   $("#backButton").click(pageBack);
 
   function checkLengthFirstPage(inputField) {
-    console.log($(inputField).attr("id"));
-    if ($(inputField).attr("id") == "first_name") {
+    var input = $(inputField);
+    if (input.attr("id") == "first_name") {
       console.log("to jest usernmae");
-      if ($(inputField).val().length >= 2) {
-        $(inputField).css("border-color", "green");
+      if (input.val().length >= 2) {
+        input.css("border-color", "green");
         firstValid = true;
       } else {
         firstValid = false;
-        $(inputField).css("border-color", "red");
+        input.css("border-color", "red");
       }
-    } else if ($(inputField).attr("id") == "last_name") {
+    } else if (input.attr("id") == "last_name") {
       console.log("to jest nazwisko");
-      if ($(inputField).val().length >= 2) {
-        $(inputField).css("border-color", "green");
-        lastnameValid = true
+      if (input.val().length >= 2) {
+        input.css("border-color", "green");
+        lastnameValid = true;
       } else {
-        lastnameValid = false
-        $(inputField).css("border-color", "red");
+        lastnameValid = false;
+        input.css("border-color", "red");
       }
-    } else if ($(inputField).attr("id") == "email") {
+    } else if (input.attr("id") == "email") {
       console.log("to jest email");
       if (
-        $(inputField).val().length >= 6 &&
-        $(inputField).val().includes("@") &&
-        $(inputField).val().includes(".")
+        input.val().length >= 6 &&
+        input.val().includes("@") &&
+        input.val().includes(".")
       ) {
-        $(inputField).css("border-color", "green");
-        emailValid = true 
+        input.css("border-color", "green");
+        emailValid = true;
       } else {
-        emailValid = false
-        $(inputField).css("border-color", "red");
+        emailValid = false;
+        input.css("border-color", "red");
       }
-    } else if ($(inputField).attr("id") == "phone_number") {
+    } else if (input.attr("id") == "phone_number") {
       console.log("to jest numer telefonu");
-      if ($(inputField).val().length == 9) {
-        $(inputField).css("border-color", "green");
-        phoneValid = true 
+      if (input.val().length == 9) {
+        input.css("border-color", "green");
+        phoneValid = true;
       } else {
         phoneValid = false;
-        $(inputField).css("border-color", "red");
+        input.css("border-color", "red");
       }
     }
-    console.log(validPoints)
+  }
+
+  function checkLengthSecondPage(inputField) {
+    var input = $(inputField);
+    var inputId = $(inputField).attr("id");
+    var inputLength = $(inputField).val().length;
+    if ($(inputField).attr("id") == "street") {
+      if (inputLength >= 5 && inputLength <= 50) {
+        input.css("border-color", "green");
+        streetValid = true;
+      } else {
+        input.css("border-color", "red");
+        streetValid = false;
+      }
+    } else if (inputId == "city") {
+      if (inputLength >= 4 && inputLength <= 50) {
+        input.css("border-color", "green");
+        cityValid = true;
+      } else {
+        cityValid = false;
+        input.css("border-color", "red");
+      }
+    } else if (inputId == "zip_code") {
+      if (inputLength == 6) {
+        input.css("border-color", "green");
+        zipValid = true;
+      } else {
+        zipValid = false;
+        input.css("border-color", "red");
+      }
+    } else if (inputId == "state") {
+      if (inputLength >= 4 && inputLength <= 50) {
+        input.css("border-color", "green");
+        stateValid = true;
+      } else {
+        stateValid = false;
+        input.css("border-color", "red");
+      }
+    } else if (inputId == "country") {
+      if (inputLength >= 4 && inputLength <= 50) {
+        input.css("border-color", "green");
+        countryValid = true;
+      } else {
+        countryValid = false;
+        input.css("border-color", "red");
+      }
+    }
   }
 
   $("input").focusout(function () {
-    checkLengthFirstPage(this);
+    if (pageNumber == 1) {
+      checkLengthFirstPage(this);
+    }
+    if (pageNumber == 2) {
+      checkLengthSecondPage(this);
+    }
   });
   $("input").focus(function () {
     $(this).css("border-color", "black");
