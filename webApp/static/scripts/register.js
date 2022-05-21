458a300b8be203abda76b9ba23aff2e35b011655
Tomasz Ogrodnik <togrodnik2006@gmail.com>
2022-05-21 08:45:35 +0200

    new Valid Register

--- webApp/static/scripts/register.js
+++ webApp/static/scripts/register.js
@@ -1,31 +1,23 @@
 $(document).ready(function () {
-  var nextButton = document.getElementById("nextButton");
   var pageNumber = 1;
 
-
-// function checkNextPage {
-// 
-// }
-
-function checkPage() {
-  if (pageNumber == 1 ) {
-    $("#0-1").show();
-    $("#0-2").hide();
-    $("#0-3").hide();
-    $("#backButton").hide()
-  }
-  else if (pageNumber == 2) {
-  $("#0-1").hide();
-  $("#0-2").show();
-  $("#0-3").hide();
-  $("#backButton").show();
-  } 
-  else if (pageNumber == 3) {
-  $("#0-1").hide();
-  $("#0-2").hide();
-  $("#0-3").show();
-  } 
-  else if (pageNumber == 4) {
+  function checkPage() {
+    if (pageNumber == 1) {
+      $("#0-1").show();
+      $("#0-2").hide();
+      $("#0-3").hide();
+      $("#backButton").hide();
+    } else if (pageNumber == 2) {
+      $("#0-1").hide();
+      $("#0-2").show();
+      $("#0-3").hide();
+      $("#backButton").show();
+    } else if (pageNumber == 3) {
+      $("#0-1").hide();
+      $("#0-2").hide();
+      $("#0-3").show();
+      $("#nextButton").val() = "submit"
+    } else if (pageNumber == 4) {
       $("#register-form").submit();
     }
   }
@@ -49,50 +41,47 @@ function checkPage() {
   $("#nextButton").click(pageNext);
   $("#backButton").click(pageBack);
 
-
-
-
-  function checkLength(inputField) {
-    console.log($(inputField).attr('id'));
-    if ($(inputField).attr('id') == "first_name") {
-      console.log("to jest usernmae")
+  function checkLengthFirstPage(inputField) {
+    console.log($(inputField).attr("id"));
+    if ($(inputField).attr("id") == "first_name") {
+      console.log("to jest usernmae");
       if ($(inputField).val().length >= 2) {
-        $(inputField).css("background-color", "green")
+        $(inputField).css("border-color", "green");
+      } else {
+        $(inputField).css("border-color", "red");
       }
-      else {
-        $(inputField).css("background-color", "white")
-      }
-    }
-    else if ($(inputField).attr('id') == "last_name") {
+    } else if ($(inputField).attr("id") == "last_name") {
       console.log("to jest nazwisko");
       if ($(inputField).val().length >= 2) {
-        $(inputField).css("background-color", "green")
+        $(inputField).css("border-color", "green");
+      } else {
+        $(inputField).css("border-color", "red");
       }
-      else {
-        $(inputField).css("background-color", "white")
+    } else if ($(inputField).attr("id") == "email") {
+      console.log("to jest email");
+      if (
+        $(inputField).val().length >= 6 &&
+        $(inputField).val().includes("@") &&
+        $(inputField).val().includes(".")
+      ) {
+        $(inputField).css("border-color", "green");
+      } else {
+        $(inputField).css("border-color", "red");
       }
-    }
-    else if($(inputField).attr('id') == "email") {
-      console.log("to jest email")
-      console.log($(inputField).attr('id').focusout)
-      if($(inputField).val().length >= 6 && $(inputField).val().includes("@") && $(inputField).val().includes(".")) {
-        console.log("jest okej")
-        $(inputField).css("background-color", "green")
-      }
-      else {
-        $(inputField).css("background-color", "white")
+    } else if ($(inputField).attr("id") == "phone_number") {
+      console.log("to jest numer telefonu");
+      if ($(inputField).val().length == 9) {
+        $(inputField).css("border-color", "green");
+      } else {
+        $(inputField).css("border-color", "red");
       }
     }
   }
 
-  $('input').focusout(function() {
+  $("input").focusout(function () {
     checkLength(this);
-  })
-  $('input').focus(function() {
-    $(this).css("background-color", "white")
-  })
-  // $("#register-button").click(function() {
-  //   checkLength();
-  // })
+  });
+  $("input").focus(function () {
+    $(this).css("border-color", "black");
+  });
 });
-
