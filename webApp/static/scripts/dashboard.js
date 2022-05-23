3c10ede8d5f6a04ddd9987d3d5a5392e02893362
Tomasz Ogrodnik <togrodnik2006@gmail.com>
2022-05-23 23:51:25 +0200

    dashboard-updated-reupload!

---
+++ webApp/static/scripts/dashboard.js
@@ -0,0 +1,18 @@
+$(".0-1").mouseenter(function () {
+  $(".br-0-1").css("width", "70px");
+});
+$(".0-1").mouseleave(function () {
+  $(".br-0-1").css("width", "30px");
+});
+$(".0-2").mouseenter(function () {
+  $(".br-0-2").css("width", "70px");
+});
+$(".0-2").mouseleave(function () {
+  $(".br-0-2").css("width", "30px");
+});
+$(".0-3").mouseenter(function () {
+  $(".br-0-3").css("width", "70px");
+});
+$(".0-3").mouseleave(function () {
+  $(".br-0-3").css("width", "30px");
+});
